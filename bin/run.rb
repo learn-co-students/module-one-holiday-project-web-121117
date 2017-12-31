d9793fab8ae824a030d54c7391f9b9c6d952261c
johneckert <johnteckert@gmail.com>
2017-12-31 10:27:43 -0500

    fixed run format and added website

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,4 @@
 require_relative '../config/environment'
-# require_relative 'helper_methods.rb'
 
 
 lunch_time = CommandLineInterface.new
--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ require_relative '../lib/style.rb'
 require_relative '../lib/restaurant_style.rb'
 require_relative '../lib/command_line_interface.rb'
 
-#turned off SQL Messages to console
-# ActiveRecord::Base.logger.level = 1
+turned off SQL Messages to console
+ActiveRecord::Base.logger.level = 1
 
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
